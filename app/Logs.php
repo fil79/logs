b475d9c4df6be98e00e07d48f290e6cd33d951bd
fmoreira <filipemoreira79@gmail.com>
2016-01-20 18:06:46 +0000

    Updated files

---
+++ app/Logs.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Logs extends Model
+{
+    
+	protected $table = 'logs';
+
+
+    public function GetLogs(){
+
+    	return "MODEL";
+
+    }
+}
