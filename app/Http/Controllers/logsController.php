cbe4b5a2d18b4d19e9bd30ee7d9ff2259ed2f063
fmoreira <filipemoreira79@gmail.com>
2016-01-20 16:26:30 +0000

    Logs Controller

---
+++ app/Http/Controllers/logsController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+
+class logsController extends Controller
+{
+    
+    public function __construct()
+    {
+        
+    }
+
+    public function getLogs()
+    {
+    	return '----';
+    }
+}
+
+
+?>
